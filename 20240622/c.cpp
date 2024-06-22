a9a60fcf22251f30d0ec629f19214e6b13f4433c
KakinokiKanta <kanta12621@icloud.com>
2024-06-22 22:45:45 +0900

    c問題模範解答

--- 20240622/c.cpp
+++ 20240622/c.cpp
@@ -1,93 +1,21 @@
-// g++ c.cpp -o c
-// cat input.txt | ./c ./c < input.txt
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
-
-enum Direction {
-  Top = 0,
-  Top_Right = 1,
-  Right = 2,
-  Bottom_Right = 3,
-  Bottom = 4,
-  Bottom_Left = 5,
-  Left = 6,
-  Top_Left = 7,
-  Center = 8,
-};
-
-Direction judgeDirection(int sx, int sy, int tx, int ty) {
-  Direction direction;
-  if (sx < tx) {
-    if (sy < ty) {
-      direction = Top_Right;
-      return direction;
-    } else if (sy == ty) {
-      direction = Right;
-      return direction;
-    } else {
-      direction = Bottom_Right;
-      return direction;
+int main() {
+    long Sx, Sy, Tx, Ty;
+    cin >> Sx >> Sy >> Tx >> Ty;
+    // タイルの左側に寄せておく
+    if ((Sx + Sy) % 2 == 1) {
+        --Sx;
     }
-  }
-  if (sx == tx) {
-    if (sy < ty) {
-      direction = Top;
-      return direction;
-    } else if (sy == ty) {
-      direction = Center;
-      return direction;
-    } else {
-      direction = Bottom;
-      return direction;
+    if ((Tx + Ty) % 2 == 1) {
+        --Tx;
     }
-  }
-  if (sx > tx) {
-    if (sy < ty) {
-      direction = Top_Left;
-      return direction;
-    } else if (sy == ty) {
-      direction = Left;
-      return direction;
-    } else {
-      direction = Bottom_Left;
-      return direction;
-    }
-  }
-  return direction;
-}
-
-int main(void){
-  Direction direction;
-  long long sx, sy;
-  cin >> sx >> sy;
-
-  long long tx, ty;
-  cin >> tx >> ty;
-
-  direction = judgeDirection(sx, sy, tx, ty);
-
-  long long fee = 0;
-
-  if (judgeDirection(sx, sy, tx, ty) == Top_Left || judgeDirection(sx, sy, tx, ty) == Top || judgeDirection(sx, sy, tx, ty) == Top_Right) {
-    fee += (ty - sy);
-  } else {
-    fee += (sy - ty);
-  }
-
-  if (judgeDirection(sx, sy, tx, ty) == Top_Left || judgeDirection(sx, sy, tx, ty) == Left || judgeDirection(sx, sy, tx, ty) == Bottom_Left) {
-    if (abs(ty - sy) < abs(tx - sx)) {
-      fee += (abs(tx - sx) - abs(ty - sy));
-    }
-    if (((sx % 2) == 1 && (sy % 2) == 0) || ((sx % 2) == 0 && (sy % 2) == 1)) {
-      fee--;
-    }
-  } else if (judgeDirection(sx, sy, tx, ty) == Top_Right || judgeDirection(sx, sy, tx, ty) == Right || judgeDirection(sx, sy, tx, ty) == Bottom_Right) {
-    if (abs(ty - sy) < abs(tx - sx)) {
-      fee += (abs(tx - sx) - abs(ty - sy));
-    }
-  }
-
-  cout << fee << endl;
-
-  return 0;
+    // 答えは (|Sy - Ty| + max(|Sx - Tx|, |Sy - Ty|)) / 2
+    long Dx = abs(Sx - Tx);
+    long Dy = abs(Sy - Ty);
+    
+    cout << (Dy + max(Dx, Dy)) / 2 << endl;
+    return 0;
 }
